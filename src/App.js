cc54a4408ca3664c39a28c2de2177b041479dfc9
dejvis06 <dejvis.beqiraj@fullscreendigital.ro>
2024-05-17 14:13:56 +0200

    create reusable header component and pass props

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
-
-function Header() {
-  return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  );
-}
+import Header from "./Header.js";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header
+        logo={logo}
+        paragraph={[
+          "Edit ",
+          <code key="code">src/App.js</code>,
+          " and save to reload.",
+        ]}
+        link={{
+          href: "https://reactjs.org",
+          text: "Learn React",
+        }}
+      />
     </div>
   );
 }
