b9a9b31b402d764248a3c205baecd534d9e01959
dejvis06 <dejvis.beqiraj@fullscreendigital.ro>
2024-05-18 17:16:18 +0200

    export default components

---
+++ react-essentials/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import reactCoreConceptsImage from "../assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+function generateRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[generateRandomInt(2)];
+
+  return (
+    <header>
+      <img src={reactCoreConceptsImage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+export default Header;
