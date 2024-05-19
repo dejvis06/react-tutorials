7c2cadd5f344ca6f69373cfd6f7002b272925013
dejvis06 <dejvis.beqiraj@fullscreendigital.ro>
2024-05-19 15:27:58 +0200

    create Player component

---
+++ tic-tac-toe/src/components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  // Save or Edit cases
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  // Handle player name components
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
